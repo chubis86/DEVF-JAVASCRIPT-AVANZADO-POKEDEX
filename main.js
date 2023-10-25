fea1b5d48f36b171765772b70912a0f69a18a585
EFREN NARVAEZ <efrennm86@gmail.com>
2023-10-24 23:28:40 -0600

    Falta la paginación y el modal

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
-const api = "https://pokeapi.co/api/v2/pokemon/?limit=1 ";
+const api = "https://pokeapi.co/api/v2/pokemon/?limit=1500";
 const buscador=document.querySelector('#buscador');
 const tarjetas=document.querySelector('#tarjetas');
 
-const x = cargarDatos();
-console.log(x.next);
+//Esto es para saber cuántos pokemons son y dividirlos en secciones de 100
+
+
+
+
+
+
+
+
+cargarDatos();
+
 //Función para obtener el listado general de los pokemons
 async function cargarDatos() {
-	const resultado = await fetch(
-		api/* ,
-		{
-			method: 'GET',
-			headers: {
-				'x-rapidapi-host': 'carbonfootprint1.p.rapidapi.com',
-				'x-rapidapi-key': 'your_api_key'
-			}
-		} */
-	);
+	const resultado = await fetch(api);
 	if (!resultado.ok) {
 		throw new Error(`HTTP error! status: ${resultado.status}`);
 	}
 	const resultado2 = await resultado.json();
-    /* const datos = resultado2.results;
-    imprimir(datos); */
-    return resultado2;    
+    const datos = resultado2.results;
+    imprimir(datos);
+        
 }
 
 //Función para obtener las imágenes de cada pokemon e imprimir sus tarjetas en pantalla
-function imprimir(datos){
+async function imprimir(datos){
     //buscador.innerHTML=datos;
     let tarjeta='';
     for(let x in datos){
         //Datos de la imagen
         let imagenURL= datos[x].url;
-        let imagen = imagenPokemon(imagenURL);
-        console.log(imagen);
+		//Obtenemos la imagen de la url
+        let imagen = await fetch(imagenURL);
+        let imagen2 = await imagen.json();
+		
         tarjeta+=
         `<div class='tarjeta'>
             <div class='tarjetaTitulo'>
                 ${datos[x].name}
             </div>    
             <div class='tarjetaImagen'>
-                <img src='${imagen}'>
+                <img class='tarjetaImagenPokemon' src='${imagen2.sprites.other.home.front_default}'>
             </div>
-        </div> `;
+        </div> `; 
         
     }
     tarjetas.innerHTML=tarjeta;
@@ -51,16 +53,7 @@ function imprimir(datos){
 
 //Función para obtener la imagen de los pokemons
 async function imagenPokemon(url) {
-	const resultado = await fetch(
-		url/* ,
-		{
-			method: 'GET',
-			headers: {
-				'x-rapidapi-host': 'carbonfootprint1.p.rapidapi.com',
-				'x-rapidapi-key': 'your_api_key'
-			}
-		} */
-	);
+	const resultado = await fetch(url);
 	if (!resultado.ok) {
 		throw new Error(`HTTP error! status: ${resultado.status}`);
 	}
