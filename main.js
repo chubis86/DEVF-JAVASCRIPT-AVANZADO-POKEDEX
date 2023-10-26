37e994fcb3d487de731165c9e3bd80fba8edeecc
EFREN NARVAEZ <efrennm86@gmail.com>
2023-10-26 01:24:17 -0600

    POKEAPI con carga asíncrona y buscador - FALTA TEMRINAR EL MODAL

--- main.js
+++ main.js
@@ -1,64 +1,139 @@
 const api = "https://pokeapi.co/api/v2/pokemon/?limit=1500";
-const buscador=document.querySelector('#buscador');
+//const api = "https://pokeapi.co/api/v2/pokemon/";
+//const buscador=document.querySelector('#buscadorBoton');
+const buscador=document.querySelector('#buscadorInput');
 const tarjetas=document.querySelector('#tarjetas');
-
-//Esto es para saber cuántos pokemons son y dividirlos en secciones de 100
-
+let pokemones = [];
 
 
 
-
-
-
-
+//Empezamos la renderización
 cargarDatos();
 
 //Función para obtener el listado general de los pokemons
 async function cargarDatos() {
-	const resultado = await fetch(api);
+    tarjetas.innerHTML='Cargando...';
+    const resultado = await fetch(api);
 	if (!resultado.ok) {
 		throw new Error(`HTTP error! status: ${resultado.status}`);
 	}
 	const resultado2 = await resultado.json();
     const datos = resultado2.results;
-    imprimir(datos);
+    //Aquí ordenamos y mandamos a imprimir
+    const datos2 = datos.map(async (dato, index) =>{
+        let arreglo=[];
+        arreglo[0] = index+1;
+        arreglo[1] = dato.name.toUpperCase();
+        let imagen = dato.url;
+        let imagen2 = await fetch(imagen);
+        let imagen3 = await imagen2.json();
+        arreglo[2] = imagen3.sprites.front_default;
+        
+        return arreglo;
+        
+    });
+    Promise.all(datos2).then(data => {
+        pokemones=data;
+    });    
         
 }
 
-//Función para obtener las imágenes de cada pokemon e imprimir sus tarjetas en pantalla
-async function imprimir(datos){
-    //buscador.innerHTML=datos;
-    let tarjeta='';
-    for(let x in datos){
-        //Datos de la imagen
-        let imagenURL= datos[x].url;
-		//Obtenemos la imagen de la url
-        let imagen = await fetch(imagenURL);
-        let imagen2 = await imagen.json();
-		
-        tarjeta+=
-        `<div class='tarjeta'>
-            <div class='tarjetaTitulo'>
-                ${datos[x].name}
-            </div>    
-            <div class='tarjetaImagen'>
-                <img class='tarjetaImagenPokemon' src='${imagen2.sprites.other.home.front_default}'>
-            </div>
-        </div> `; 
-        
+//Damos un tiempo para cargar la lista completa
+setTimeout(()=>{
+    console.log(pokemones); 
+    imprimir(pokemones);
     }
-    tarjetas.innerHTML=tarjeta;
+    ,5000);
+
+
+function imprimir(arreglo){
+    let tarjeta='';
+    arreglo.forEach(pokemon =>{
+        
+        tarjeta +=
+        `<div class='tarjeta' data='${pokemon[0]}'>
+            <div class='tarjetaTitulo'>
+                ${pokemon[1]}
+            </div>
+                
+            <div class='tarjetaImagen'>
+                
+            
+                <img class='tarjetaImagenPokemon'
+        `;
+        tarjeta += pokemon[2]==null ?  `src="/imagenes/error.jpg"` : `src="${pokemon[2]}" `;
+        tarjeta += `    
+                />
+                
+            </div>
+        </div> `;
+    });
+       
+     
+    
+    setTimeout(() => {
+        tarjetas.innerHTML=tarjeta;
+        //Le asignamos listeners a las tarjetas
+        darAccionTarjetas();
+    }, 1000);
+
     
 }
 
-//Función para obtener la imagen de los pokemons
-async function imagenPokemon(url) {
-	const resultado = await fetch(url);
-	if (!resultado.ok) {
-		throw new Error(`HTTP error! status: ${resultado.status}`);
-	}
-	const datos = await resultado.json();
-    //return datos.results;
-    console.log(datos);
+//Funcionalidad del buscador
+buscador.addEventListener('keyup', e => {
+    //console.log(buscador.value);
+    //console.log(pokemones);
+    let arregloBusqueda = pokemones.filter(pokemon => {
+        /* console.log(pokemon[1]);
+        console.log(pokemon[1].indexOf(buscador.value)); */
+        if(pokemon[1].indexOf(buscador.value.toUpperCase()) > -1){
+            return pokemon;
+        }
+    });
+    //console.log(arregloBusqueda);    
+    imprimir(arregloBusqueda);
+});
+
+
+//Aquí asignamos funcionalidad a las tarjetas
+function darAccionTarjetas(){
+    //Se tiene que llamar desde la función asíncrona que es donde se crean todas las tarjetas. De otro modo, esta función no encuentra ningún elemento con clase 'tarjeta'
+    
+    const tarjetaSelector=document.querySelectorAll('.tarjeta');
+    tarjetaSelector.forEach(tarjeta =>{
+        tarjeta.addEventListener('click',async e =>{
+            let posicion = tarjeta.getAttribute('data');
+            
+            document.querySelector('.modal-title').innerHTML = pokemones[posicion-1][1];
+            let apiPokemon='https://pokeapi.co/api/v2/pokemon/' + pokemones[posicion][0];
+            let informacionPokemon = await fetch(apiPokemon);
+            let informacionPokemon2 = await informacionPokemon.json();
+            let cadena = 
+            `<div class='informacionColumnaIzquierda'>
+                <span>HABILIDADES:</span>
+            `;    
+                informacionPokemon2.abilities.forEach(habilidad =>{
+                    cadena+= `<span>${habilidad.ability.name}</span>;`;
+                });    
+            cadena+=
+            `</div>
+            <div class='informacionColumnaDerecha'>
+                <img `;
+                cadena+=pokemones[posicion][2]==null ?  `src="/imagenes/error.jpg"` : `src="${pokemones[posicion][2]}" `;
+            
+            cadena+=
+            `/>    
+            </div>
+            `;
+
+            document.querySelector('.modal-body').innerHTML = cadena;
+            
+            $('.modal').modal('show');
+        });
+    });
 }
 
+$(".cerrarModal").click(function(){
+    $(".modal").modal('hide')
+});
